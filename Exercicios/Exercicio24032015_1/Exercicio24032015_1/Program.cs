8b33c386c346ee23a62650fedf1b10b58de31e5f
math1997 <math.ffc@hotmail.com>
2015-03-24 17:56:17 -0300

    oi

---
+++ Exercicios/Exercicio24032015_1/Exercicio24032015_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio24032015_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double num;
+            double soma = 0;
+            double maior = -1;
+            double menor = 10000;
+
+
+
+            Console.WriteLine("Digite a quantidade:");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Digite os numeros: ");
+                num = int.Parse(Console.ReadLine());
+                soma += num;
+
+                if (num > maior);
+                {
+                    
+                }
+              
+
+                    
+                
+            }
+        }
+    }
+}
