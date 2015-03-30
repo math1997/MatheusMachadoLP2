f58b70ba454be4e49a9ca85a60c6468040fce0cf
math1997 <math.ffc@hotmail.com>
2015-03-30 00:49:52 -0300

    exercicio

--- Exercicios/Exercicio24032015_1/Exercicio24032015_1/Program.cs
+++ Exercicios/Exercicio24032015_1/Exercicio24032015_1/Program.cs
@@ -9,7 +9,7 @@ namespace Exercicio24032015_1
     class Program
     {
         static void Main(string[] args)
-        {
+        {// criação variaveis
             int n;
             double num;
             double soma = 0;
@@ -17,10 +17,10 @@ namespace Exercicio24032015_1
             double menor = 10000;
 
 
-
+            //digitando a quantidade
             Console.WriteLine("Digite a quantidade:");
             n = int.Parse(Console.ReadLine());
-
+            //laço para calculos
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine("Digite os numeros: ");
@@ -29,12 +29,19 @@ namespace Exercicio24032015_1
 
                 if (num > maior);
                 {
-                    
+                    maior = num;   
                 }
-              
+                if (num < menor)
+                {
+                    menor = num;
+                }
 
                     
-                
+            }
+
+            Console.WriteLine("Somando fica: {0}", soma);
+            Console.WriteLine("o menor é: {0}", menor);
+            Console.WriteLine("o maior é: {0}", menor);
             }
         }
     }
