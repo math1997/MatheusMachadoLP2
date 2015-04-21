cfb16bbbed4040b916e653e9efa46a7eafb20408
math1997 <math.ffc@hotmail.com>
2015-04-21 20:56:42 -0300

    Commit Certo lista1

---
+++ Listas/Lista1/exercicio 2 lista 1/sytaexercicio 2 lista 1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sytaexercicio_2_lista_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+        double divisor;
+        double dv = 0;
+        double Res;
+        double resultado = 0;
+        
+
+        public void calculoComFatorial()
+        {
+         EX2 exercicio2 = new EX2 exercicio2();
+
+        Console.WriteLine("Informe o número: "); // Limite = 47
+        N = int.Parse(Console.ReadLine());
+
+        int t = 1;
+        double D;  
+        int DV;
+        int dv;
+            int D;
+        int r = 0;
+       int div;
+            int result;
+            int Res;
+            int N;
+        while (r <= N)
+        {
+            div = dv + (dv + 1);
+            D = div + (div + 1);
+            D = int.Parse();
+            DV = D + (D + 1);
+            dv = DV;
+
+            Res  = exercicio2.calculaFatorial(t) / div - exercicio2.calculaFatorial(t++) / D;
+            result = Res;
+
+            N--; 
+
+            if (r <= N)
+            {
+                Res = result + exercicio2.calculaFatorial(t++) / DV;
+                result = Res;
+
+                N--;
+            }
+
+            t++;
+            r++;
+        }
+     
+        Console.WriteLine("O resultado é: " + Res);
+        Console.WriteLine();
+        }
+    }
+}
---
+++ Listas/Lista1/exercicio 4 lista 1/exercicio 4 lista 1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio 4 lista 1
+{
+    class program
+
+    {
+        // Variaveis
+        int player1;
+        int player2;
+        int EndMatch = 1;
+        int finish = 0;
+        int totalPointsPlayer1 = 0;
+        int totalPointsPlayer2 = 0;
+
+        public void Vencedor()
+        {
+            // Laço
+            while (EndMatch != finish)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ponto Player 1: ");
+                player1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ponto Player 2: ");
+                player2 = int.Parse(Console.ReadLine());
+
+                totalPointsPlayer1 += player1;
+                totalPointsPlayer2 += player2;
+
+                // Condição
+                if (totalPointsPlayer1 == 21 && totalPointsPlayer2 < 20)
+                {
+                    EndMatch = finish;
+                }
+                else if (totalPointsPlayer2 == 21 && totalPointsPlayer1 < 20)
+                {
+                    EndMatch = finish;
+                }
+
+                else if (totalPointsPlayer1 > 21 && totalPointsPlayer1 >= totalPointsPlayer2 + 2)
+                {
+                    EndMatch = finish;
+                }
+                else if (totalPointsPlayer2 > 21 && totalPointsPlayer2 >= totalPointsPlayer1 + 2)
+                {
+                    EndMatch = finish;
+                }
+            }
+
+            if (totalPointsPlayer1 > totalPointsPlayer2)
+            {
+                Console.WriteLine("O vencedor foi o Player 1 com " + totalPointsPlayer1 + " pontos");
+            }
+            else
+            {
+                Console.WriteLine("O vencedor foi o Player 2 com " + totalPointsPlayer2 + " pontos");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
