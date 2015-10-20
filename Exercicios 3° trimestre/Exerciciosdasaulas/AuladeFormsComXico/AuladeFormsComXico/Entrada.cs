444bbe057c0d75e88b4609a8de7646df99a793f5
matheus <math.ffc@hotmail.com>
2015-10-20 17:51:22 -0300

    a

---
+++ Exercicios 3° trimestre/Exerciciosdasaulas/AuladeFormsComXico/AuladeFormsComXico/Entrada.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuladeFormsComXico
+{
+    public partial class Entrada : Form
+    {
+        public string Dado;
+
+        public Entrada()
+        {
+            InitializeComponent();
+                    
+
+        }
+
+        private void Entrada_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                Dado = textBox1.Text;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Preencha o text box");
+            }
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Exercicios 3° trimestre/Exerciciosdasaulas/AuladeFormsComXico/AuladeFormsComXico/Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuladeFormsComXico
+{
+    public partial class Main : Form
+    {
+        string buffer;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Entrada F = new Entrada();
+            F.ShowDialog();
+            buffer = F.Dado; 
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = buffer;
+        }
+    }
+}
