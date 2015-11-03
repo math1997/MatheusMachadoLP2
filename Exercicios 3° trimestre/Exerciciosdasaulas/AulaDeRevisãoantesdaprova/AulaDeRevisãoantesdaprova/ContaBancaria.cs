58464f65a7bbb9f4f607a5c4beb36e15815aeb42
matheus <math.ffc@hotmail.com>
2015-11-03 17:59:57 -0300

    revisao

---
+++ Exercicios 3° trimestre/Exerciciosdasaulas/AulaDeRevisãoantesdaprova/AulaDeRevisãoantesdaprova/ContaBancaria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaDeRevisãoantesdaprova
+{
+    class ContaBancaria
+    {
+        public string cliente { get; set; }
+        public int num_conta  { get; set; }
+        public double saldo  { get; set; }
+        public double novosaldo { get; set; }
+        public void depositar(double valor)
+        {
+            novosaldo = saldo + valor;  
+        }
+        public void sacar( double valor )
+        {
+            if (saldo - valor >= 0)
+            {
+               novosaldo = saldo - valor; 
+            }
+            else
+            {
+                Console.WriteLine("O valor sacado vai deixar seu saldo negativo");
+            }
+        }
+       
+    }
+
+}
---
+++ Exercicios 3° trimestre/Exerciciosdasaulas/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Digite Poppy para inserir uma nova pessoa na tabela, e digite John Cena para consultar um nome registrado");
+             string op = Console.ReadLine();
+
+            List<Pessoa> pessoas = new List<Pessoa>();         
+            if (op == "Poppy")
+            {
+              Pessoa P = new Pessoa();
+              Console.Write("Digite o seu nome: ");
+              P.Nome = Console.ReadLine();  
+              Console.Write("Digite o seu sobrenome: ");
+              P.Sobrenome = Console.ReadLine();
+              Console.Write("Digite o seu peso: ");
+              P.Peso = double.Parse(Console.ReadLine());
+              Console.Write("Digite o seu altura: ");
+              P.Altura = double.Parse(Console.ReadLine());
+              Console.Write("Digite o seu telefone: ");
+              P.Telefone = int.Parse(Console.ReadLine());
+              pessoas.Add(P);
+
+              string strConn = "Server=localhost;Database=test;Uid=root;Pwd=";
+              MySqlConnection conn = new MySqlConnection(strConn);
+
+              MySqlCommand command = new MySqlCommand();
+              command.Connection = conn;
+
+              command.CommandText = String.Format("INSERT INTO Pessoa (Nome,Sobrenome,Peso,Altura,Telefone) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')", P.Nome, P.Sobrenome , P.Peso , P.Altura , P.Telefone);
+              conn.Open();
+              command.ExecuteNonQuery();
+              conn.Close();
+            }
+
+            else if(op == "John Cena")
+            {
+                    
+            }
+        }
+    }
+}
